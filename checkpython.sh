38894180c1e9e38381bcbdfb48c2bc8b6b9625c1
Christopher S. Penn <cspenn@gmail.com>
2025-08-22 07:19:01 -0400

    Clean up and initial commit

--- checkpython.sh
+++ checkpython.sh
@@ -1,1 +1,32 @@
-/users/cspenn/code/masterlibraries/checkpython.sh
+#!/bin/zsh
+# start checkpython.sh
+# This script runs all static analysis and formatting checks for the project.
+# You must never modify the contents of checkpython.sh under any circumstances
+
+echo "Beginning cleaning."
+
+pyclean .
+
+echo "Beginning ruff."
+
+
+ruff check . --fix
+ruff format
+
+echo "Beginning mypy."
+
+mypy .
+
+echo "Beginning bandit."
+
+bandit -r .
+
+echo "Beginning pytest."
+
+poetry run python -m pytest
+
+echo "Beginning final cleaning."
+
+pyclean .
+
+# end checkpython.sh
